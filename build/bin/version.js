60f02312de9ccbd794ab3f40c77ef0f94bd97748
DESKTOP-E77T282\hcq <653427831@qq.com>
2021-11-23 16:56:13 +0800

    docs:根据package.json中的version,生成examples/versions.json，对应就是完整的版本列表

--- build/bin/version.js
+++ build/bin/version.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 var path = require('path');
+// 从 package.json 中 获取 version
 var version = process.env.VERSION || require('../../package.json').version;
 var content = { '1.4.13': '1.4', '2.0.11': '2.0', '2.1.0': '2.1', '2.2.2': '2.2', '2.3.9': '2.3', '2.4.11': '2.4', '2.5.4': '2.5', '2.6.3': '2.6', '2.7.2': '2.7', '2.8.2': '2.8', '2.9.2': '2.9', '2.10.1': '2.10', '2.11.1': '2.11', '2.12.0': '2.12', '2.13.2': '2.13', '2.14.1': '2.14' };
+// 如果 content 中不存在该版本，则新增
 if (!content[version]) content[version] = '2.15';
+// 将版本列表数据写入到 versions.json 中
 fs.writeFileSync(path.resolve(__dirname, '../../examples/versions.json'), JSON.stringify(content));
