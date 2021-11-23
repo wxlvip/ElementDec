b422f0040c2bf1c1cb4d320e029253fd14651fe9
DESKTOP-E77T282\hcq <653427831@qq.com>
2021-11-23 16:56:51 +0800

    docs:通过 gulp 把packages/theme-chalk下的所有scss文件编译为css

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,19 +1,19 @@
 'use strict';
-
+// 把packages/theme-chalk下的所有scss文件编译为css，按需加载时需要单独引入样式
 const { series, src, dest } = require('gulp');
-const sass = require('gulp-dart-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cssmin = require('gulp-cssmin');
+const sass = require('gulp-dart-sass'); // 编译gulp工具
+const autoprefixer = require('gulp-autoprefixer'); // 添加厂商前缀
+const cssmin = require('gulp-cssmin'); // 压缩css
 
 function compile() {
-  return src('./src/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(autoprefixer({
+  return src('./src/*.scss') // src下的所有scss文件
+    .pipe(sass.sync().on('error', sass.logError)) // 把scss文件编译成css
+    .pipe(autoprefixer({ // 基于目标浏览器版本，添加厂商前缀 根据浏览器的不同版本自动处理浏览器的前缀，实现css浏览器的兼容。
       overrideBrowserslist: ['ie > 9', 'last 2 versions'],
       cascade: false
     }))
-    .pipe(cssmin())
-    .pipe(dest('./lib'));
+    .pipe(cssmin()) // 压缩css
+    .pipe(dest('./lib')); // 输出到lib下
 }
 
 function copyfont() {
