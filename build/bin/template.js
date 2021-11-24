7e776473897438d26ec639d0549a2a7ea0db6283
DESKTOP-E77T282\hcq <653427831@qq.com>
2021-11-24 11:53:22 +0800

    docs:新增注释，模板文件变化后执行国际化处理

--- build/bin/template.js
+++ build/bin/template.js
@@ -1,6 +1,10 @@
+/**
+ * tpl后缀模板文件的处理 当文件有所改变的时候，执行 `npm run i18n` 进行文件国际化处理
+ */
 const path = require('path');
+// 加载页面模板
 const templates = path.resolve(process.cwd(), './examples/pages/template');
-
+// Chokidar 是一个极简高效的跨平台文件查看器。
 const chokidar = require('chokidar');
 let watcher = chokidar.watch([templates]);
 
