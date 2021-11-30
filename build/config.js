d55043609ed105582ebdeb228d4353401f0fe5c0
DESKTOP-E77T282\hcq <653427831@qq.com>
2021-11-30 13:26:11 +0800

    feat:根据自己的 npm 包名处理打包后的文件名称，涉及当前几个文件，需结合说明文档处理

--- build/config.js
+++ build/config.js
@@ -9,34 +9,34 @@ var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
-  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`;
+  externals[`element-ui/packages/${key}`] = `wxl-ui/lib/${key}`;
 });
 
-externals['element-ui/src/locale'] = 'element-ui/lib/locale';
+externals['element-ui/src/locale'] = 'wxl-ui/lib/locale';
 utilsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`;
+  externals[`element-ui/src/utils/${file}`] = `wxl-ui/lib/utils/${file}`;
 });
 mixinsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`;
+  externals[`element-ui/src/mixins/${file}`] = `wxl-ui/lib/mixins/${file}`;
 });
 transitionList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`;
+  externals[`element-ui/src/transitions/${file}`] = `wxl-ui/lib/transitions/${file}`;
 });
 
 externals = [Object.assign({
   vue: 'vue'
 }, externals), nodeExternals()];
-
 exports.externals = externals;
 
 exports.alias = {
   main: path.resolve(__dirname, '../src'),
   packages: path.resolve(__dirname, '../packages'),
   examples: path.resolve(__dirname, '../examples'),
-  'element-ui': path.resolve(__dirname, '../')
+  'element-ui': path.resolve(__dirname, '../'),
+  'wxl-ui': path.resolve(__dirname, '../')
 };
 
 exports.vue = {
